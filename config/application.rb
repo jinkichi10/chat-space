ac4baf467521227867127e66046c386666540f92
jinkichi10 <67736186+jinkichi10@users.noreply.github.com>
2020-07-06 14:24:35 +0900

    07061424

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,4 @@ module ChatSpace
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
 end
